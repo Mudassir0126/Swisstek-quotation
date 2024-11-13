8c462d14b560147638f71436bb83dd6a8dad4c6e
Mudassir <mudassirahmed13631@gmail.com>
2024-11-13 22:57:22 +0530

    Initial commit

---
+++ src/App.jsx
@@ -0,0 +1,14 @@
+// src/App.js
+import React from 'react';
+import Navbar from './components/Navbar';
+
+function App() {
+  return (
+    <div className="App">
+      <Navbar />
+      {/* Other components go here */}
+    </div>
+  );
+}
+
+export default App;
